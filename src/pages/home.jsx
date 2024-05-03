123e809e217e6e7f5762d6dbfefc8dff8b7955aa
steeven-js <jacques.steeven@gmail.com>
2024-05-03 13:12:54 -0400

    chore: Update package dependencies and add dotenv library

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,7 @@ import TravelLandingView from 'src/sections/_travel/view/travel-landing-view';
 // ----------------------------------------------------------------------
 
 export default function HomePage() {
+  console.log(import.meta.env.VITE_AUTH_API_URL)
   return (
     <>
       <Helmet>
