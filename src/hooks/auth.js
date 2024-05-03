9cdc26cace5f46fd53fd1bf9a9d94804b1af3a64
steeven-js <jacques.steeven@gmail.com>
2024-05-03 14:12:31 -0400

    chore: Refactor useAuth hook to handle API errors and fetch CSRF token

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,93 +1,104 @@
+/* eslint-disable no-useless-catch */
 /* eslint-disable no-shadow */
 /* eslint-disable react-hooks/exhaustive-deps */
 import useSWR from 'swr'
-import {useEffect} from 'react'
-import {useParams, useNavigate} from 'react-router-dom';
+import { useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom';
 
 import axios from 'src/lib/axios'
 
-export const useAuth = ({middleware, redirectIfAuthenticated} = {}) => {
+export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
   const navigate = useNavigate();
   const params = useParams();
 
-  const {data: user, error, mutate} = useSWR('/api/user', () =>
+  const { data: user, error, mutate } = useSWR('/api/user', () =>
     axios
       .get('/api/user')
       .then(res => res.data)
       .catch(error => {
         if (error.response.status !== 409) throw error
-
         mutate('/verify-email')
       }),
-  {
-    revalidateIfStale: false,
-    revalidateOnFocus: false
-  }
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false
+    }
   )
 
-  const csrf = () => axios.get('/sanctum/csrf-cookie')
-
-  const register = async ({setErrors, ...props}) => {
-    await csrf()
-    setErrors([])
-    axios
-      .post('/register', props)
-      .then(() => mutate())
-      .catch(error => {
-        if (error.response.status !== 422) throw error
-        setErrors(Object.values(error.response.data.errors).flat())
-      })
+  const fetchCsrfToken = async () => {
+    await axios.get('/sanctum/csrf-cookie')
   }
 
-  const login = async ({setErrors, setStatus, ...props}) => {
-    await csrf()
-    setErrors([])
-    setStatus(null)
-    axios
-      .post('/login', props)
-      .then(() => mutate())
-      .catch(error => {
-        if (error.response.status !== 422) throw error
-        setErrors(Object.values(error.response.data.errors).flat())
-      })
+  const handleApiError = (error, setErrors) => {
+    if (error.response && error.response.status === 422) {
+      setErrors(Object.values(error.response.data.errors).flat())
+    } else {
+      throw error
+    }
   }
 
-  const forgotPassword = async ({setErrors, setStatus, email}) => {
-    await csrf()
+  const register = async ({ setErrors, ...props }) => {
+    await fetchCsrfToken()
+    setErrors([])
+    try {
+      await axios.post('/register', props)
+      mutate()
+    } catch (error) {
+      handleApiError(error, setErrors)
+    }
+  }
+
+  const login = async ({ setErrors, setStatus, ...props }) => {
+    await fetchCsrfToken()
     setErrors([])
     setStatus(null)
-    axios
-      .post('/forgot-password', {email})
-      .then(response => setStatus(response.data.status))
-      .catch(error => {
-        if (error.response.status !== 422) throw error
-        setErrors(Object.values(error.response.data.errors).flat())
-      })
+    try {
+      await axios.post('/login', props)
+      mutate()
+    } catch (error) {
+      handleApiError(error, setErrors)
+    }
   }
 
-  const resetPassword = async ({setErrors, setStatus, ...props}) => {
-    await csrf()
+  const forgotPassword = async ({ setErrors, setStatus, email }) => {
+    await fetchCsrfToken()
     setErrors([])
     setStatus(null)
-    axios
-      .post('/reset-password', {token: params.token, ...props})
-      .then(response => navigate(`/login?reset=${  btoa(response.data.status)}`))
-      .catch(error => {
-        if (error.response.status !== 422) throw error
-        setErrors(Object.values(error.response.data.errors).flat())
-      })
+    try {
+      const response = await axios.post('/forgot-password', { email })
+      setStatus(response.data.status)
+    } catch (error) {
+      handleApiError(error, setErrors)
+    }
   }
 
-  const resendEmailVerification = ({setStatus}) => {
-    axios
-      .post('/email/verification-notification')
-      .then(response => setStatus(response.data.status))
+  const resetPassword = async ({ setErrors, setStatus, ...props }) => {
+    await fetchCsrfToken()
+    setErrors([])
+    setStatus(null)
+    try {
+      const response = await axios.post('/reset-password', { token: params.token, ...props })
+      navigate(`/login?reset=${btoa(response.data.status)}`)
+    } catch (error) {
+      handleApiError(error, setErrors)
+    }
+  }
+
+  const resendEmailVerification = async ({ setStatus }) => {
+    try {
+      const response = await axios.post('/email/verification-notification')
+      setStatus(response.data.status)
+    } catch (error) {
+      throw error
+    }
   }
 
   const logout = async () => {
-    if (!error) {
+    try {
       await axios.post('/logout')
       mutate()
+    } catch (error) {
+      throw error
     }
     window.location.pathname = '/'
   }
