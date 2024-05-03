89123dae3154cca6e4fb6cc538ac6a9698bc893b
steeven-js <jacques.steeven@gmail.com>
2024-05-03 13:16:03 -0400

    chore: Update axios.js to use hardcoded baseURL for development environment

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-  baseURL: import.meta.env.VITE_AUTH_API_URL,
+  baseURL: 'http://localhost:8000',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
